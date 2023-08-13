7e3a878cf90742167f7c58c90af0f803b22be196
matt berseth <matt.berseth@nlplogix.com>
2023-08-13 08:06:33 -0400

    Seeding repo

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # cs451-101Z-ENV
+Follow the instructions to ensure you have the computing environment configured properly.
+
+## Steps
+1. Install Ubuntu 22.04.2 LTS (WSL)
+1. Configure python 3.10
+```bash
+# launch a WSL window
+sudo apt update
+sudo apt -y upgrade
+
+# confirm python 3.10
+python3 -V
+
+# install python dev tools
+sudo apt install -y python3-pip
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+```
+1. Clone this repo.
+```bash
+git clone https://github.com/matt-berseth/cs451-101Z-ENV.git
+```
+1. Create the virtual environment and launch vscode
+```bash
+python3 -m venv .venv
+source ./.venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+code .
+```
+1. Install the following vscode packages (Python, Pylance)
+1. Launch the 'main' entry.
---
+++ main.py
@@ -0,0 +1,34 @@
+import logging
+
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
+
+
+def main():
+    # read in the iris csv file
+    logging.info("Reading 'iris.csv' ...")
+    df = pd.read_csv("./iris.csv")
+    logging.info(f"{df.shape[0]} rows, {df.shape[1]} columns.")
+    logging.info(df.describe())
+
+    # pull out X and y
+    X = df[["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]]
+    y = df[["Species"]].values
+
+    logging.info("Training the classifier ...")
+    clf = DecisionTreeClassifier(max_depth=3)
+    clf.fit(X, y)
+
+    logging.info("Making predictions ...")
+    yhat = clf.predict(X)
+
+    n_correct = np.sum(yhat == y[:, 0])
+    logging.info(f"{n_correct} correct out of {X.shape[0]}")
+
+
+if __name__ == "__main__":
+    main()
